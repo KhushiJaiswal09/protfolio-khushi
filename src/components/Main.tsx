fdb9e45e8d3a6ec3a046f65ba0d00ae8931fce0b
Khushi Jaiswal <103581045+KhushiJaiswal09@users.noreply.github.com>
2024-12-23 14:02:19 +0530

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -18,7 +18,7 @@ function Main() {
           <div className="social_icons">
             <a href="https://github.com/KhushiJaiswal09" target="_blank" rel="noreferrer"><GitHubIcon /></a>
             <a href="https://www.linkedin.com/in/khushijaiswal09/" target="_blank" rel="noreferrer"><LinkedInIcon /></a>
-            <a href="https://drive.google.com/file/d/1nDY6eGwnUfgGpHD9EWCOTjPZI8GdcZav/view?usp=sharing" target="_blank" rel="noreferrer">
+            <a href="https://drive.google.com/file/d/1j3x93qXD0iJ6UzqePMX5rNs_BB-AyBMA/view" target="_blank" rel="noreferrer">
               <FaDownload className="small-icon" />
             </a>
 
